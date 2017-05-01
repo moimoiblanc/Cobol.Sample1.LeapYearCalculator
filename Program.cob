669c60d44cf4a22aca464de9d5a3d5d8460ac42f
moimoiblanc <moimoiblanc@gmail.com>
2017-05-01 15:15:29 +0900

    入力内容を検査する機能を追加

--- Program.cob
+++ Program.cob
@@ -5,7 +5,12 @@
 000000 WORKING-STORAGE SECTION.
 000000
 000000*    閏年計算したい西暦
-000000 1   Year PIC 9(4).
+000000 1   Year.
+000000 2       InputString.
+000000 3         InputValue PIC X(4).
+000000 3         Tail PIC X(1).
+000000 2       CompleteValue PIC 9(4).
+000000 2       ErrorMessage PIC X(38).
 000000
 000000*    閏年計算のためのパラメーター
 000000 1   LeapYearCalculator.
@@ -22,7 +27,24 @@
 000000 MAIN.
 000000
 000000     DISPLAY "閏年計算したい西暦を入力して下さい".
-000000     ACCEPT Year.
+000000     ACCEPT InputString.
+000000
+000000     IF Tail NOT = X"0D" THEN
+000000         MOVE "西暦は４桁で入力する必要があります。"
+000000             TO ErrorMessage
+000000     END-IF.
+000000     IF InputValue IS NOT NUMERIC THEN
+000000         MOVE "西暦は数値で入力する必要があります。"
+000000             TO ErrorMessage
+000000     END-IF.
+000000
+000000     IF ErrorMessage NOT = "" THEN
+000000         DISPLAY ErrorMessage
+000000         COMPUTE RETURN-CODE = 20
+000000         STOP RUN
+000000     END-IF.
+000000
+000000     MOVE InputValue TO CompleteValue.
 000000
 000000     COMPUTE DivisionParameter(1) = 400.
 000000     COMPUTE ReturnCode(1) = 10.
@@ -38,8 +60,8 @@
 000000     PERFORM VARYING CurrentIndex FROM 1 BY 1
 000000         UNTIL CurrentIndex > TotalCount
 000000
-000000         IF FUNCTION MOD(
-000000             Year, DivisionParameter(CurrentIndex)) = 0 THEN
+000000         IF FUNCTION MOD(CompleteValue,
+000000             DivisionParameter(CurrentIndex)) = 0 THEN
 000000
 000000             COMPUTE RETURN-CODE = ReturnCode(CurrentIndex)
 000000             EXIT PERFORM
